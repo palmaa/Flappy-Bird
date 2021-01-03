85309a04321eee0cc0719172ebdbc945acd3cb9e
palmaa <paalma9@gmail.com>
2021-01-03 21:41:02 +0100

    Main donde se muestra la pantalla + imagenes y audio
    Main with a black screen + images and audio

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((576,1024))
+clock = pygame.time.Clock()
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    #120 == max FPS
+    clock.tick(120)
